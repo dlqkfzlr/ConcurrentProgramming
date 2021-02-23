f9355cb4a4d76b475914fed640e41492e3dd7435
dlqkfzlr <hisunwoong@gmail.com>
2021-02-24 01:04:02 +0900

    Add network request and UI features in MainActivity.kt

--- app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
+++ app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
@@ -1,18 +1,52 @@
 package m.woong.concurrentprogramming
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
-    val netDispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val factory = DocumentBuilderFactory.newInstance()
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        GlobalScope.launch(netDispatcher) {
+        GlobalScope.launch(dispatcher) {
             printCurrentThread()
         }
+        GlobalScope.launch(dispatcher) {
+            val headlines = fetchRssHeadlines()
+            val newsCount = findViewById<TextView>(R.id.newsCount)
+            GlobalScope.launch(Dispatchers.Main) {
+                newsCount.text = "Found ${headlines.size} News"
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+//        Thread.sleep(5000)
+    }
+
+    private fun fetchRssHeadlines(): List<String> {
+        val builder = factory.newDocumentBuilder()
+        val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
+        val news = xml.getElementsByTagName("channel").item(0)
+        return (0 until news.childNodes.length)
+            .map { news.childNodes.item(it) }
+            .filter { Node.ELEMENT_NODE == it.nodeType }
+            .map { it as Element }
+            .filter { "item" == it.tagName }
+            .map {
+                it.getElementsByTagName("title").item(0).textContent
+            }
     }
 }
