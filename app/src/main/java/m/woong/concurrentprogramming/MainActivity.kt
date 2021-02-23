0d3c52141cf308b2540c36f778e52c62b65b969a
dlqkfzlr <hisunwoong@gmail.com>
2021-02-23 23:06:04 +0900

    Use newSingleThreadContext as a CoroutineDispatcher

--- app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
+++ app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
@@ -1,11 +1,18 @@
 package m.woong.concurrentprogramming
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
+    val netDispatcher = newSingleThreadContext(name = "ServiceCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch(netDispatcher) {
+            printCurrentThread()
+        }
     }
 }
