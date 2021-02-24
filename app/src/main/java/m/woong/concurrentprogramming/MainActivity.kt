5b99a43186e7989b7e4d9514184d7f24fea135ce
dlqkfzlr <hisunwoong@gmail.com>
2021-02-24 18:48:54 +0900

    Add Deferred cancellation exception handling in MainActivity.kt

--- app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
+++ app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
@@ -4,43 +4,68 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.*
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
-    private val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    val feeds = listOf(
+        "https://www.npr.org/rss/rss.php?id=1001",
+        "http://rss.cnn.com/rss/cnn_topstories.rss",
+        "http://feeds.foxnews.com/foxnews/politics?format=xml",
+        "fake"
+    )
+    private val dispatcher = newFixedThreadPoolContext(2, "IO")
     private val factory = DocumentBuilderFactory.newInstance()
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        GlobalScope.launch(dispatcher) {
-//            printCurrentThread()
-        }
-        GlobalScope.launch(dispatcher) {
-            val headlines = fetchRssHeadlines()
-            val newsCount = findViewById<TextView>(R.id.newsCount)
-            GlobalScope.launch(Dispatchers.Main) {
-                newsCount.text = "Found ${headlines.size} News"
-            }
-        }
+        asyncLoadNews()
+
     }
 
     override fun onResume() {
         super.onResume()
-//        Thread.sleep(5000)
     }
 
-    private fun fetchRssHeadlines(): List<String> {
+    @SuppressLint("SetTextI18n")
+    private fun asyncLoadNews() = GlobalScope.launch {
+        val requests = mutableListOf<Deferred<List<String>>>()
+        feeds.mapTo(requests) {
+            asyncFetchRssHeadlines(it, dispatcher)
+        }
+        requests.forEach {  // 각각의 asyncFetchRssHeadlines가 모두 완료될 때까지 대기하는 코드
+            it.join()
+        }
+        val headlines = requests
+            .filter { !it.isCancelled }
+            .flatMap { it.getCompleted() }
+
+        val failed = requests.filter { it.isCancelled }
+            .size
+
+        val newsCount = findViewById<TextView>(R.id.newsCount)
+        val warnings = findViewById<TextView>(R.id.warnings)
+        val obtained = requests.size - failed
+
+        GlobalScope.launch(Dispatchers.Main) {
+            newsCount.text = "Found ${headlines.size} News" +
+                    "in ${obtained} feeds"
+            if (failed > 0) warnings.text = "Failed to fetch $failed feeds"
+        }
+    }
+
+    private fun asyncFetchRssHeadlines(
+        feed: String,
+        dispatcher: CoroutineDispatcher
+    ) = GlobalScope.async(dispatcher) {
         val builder = factory.newDocumentBuilder()
-        val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
+        val xml = builder.parse(feed)
         val news = xml.getElementsByTagName("channel").item(0)
-        return (0 until news.childNodes.length)
+        (0 until news.childNodes.length)
             .map { news.childNodes.item(it) }
             .filter { Node.ELEMENT_NODE == it.nodeType }
             .map { it as Element }
