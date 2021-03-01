6d60c19a149deca5e98117d8910905cb3a59935a
dlqkfzlr <hisunwoong@gmail.com>
2021-03-01 13:49:23 +0900

    Apply ViewBinding to MainActivity.kt

--- app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
+++ app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
@@ -2,9 +2,12 @@ package m.woong.concurrentprogramming
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import m.woong.concurrentprogramming.adapter.ArticleAdapter
+import m.woong.concurrentprogramming.databinding.ActivityMainBinding
 import m.woong.concurrentprogramming.model.Article
 import m.woong.concurrentprogramming.model.Feed
 import org.w3c.dom.Element
@@ -18,15 +21,21 @@ class MainActivity : AppCompatActivity() {
         Feed("fox", "http://feeds.foxnews.com/foxnews/politics?format=xml"),
         Feed("inv", "htt:myNewsFeed")
     )
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewAdapter: ArticleAdapter
     private val dispatcher = newFixedThreadPoolContext(2, "IO")
     private val factory = DocumentBuilderFactory.newInstance()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        asyncLoadNews()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        viewAdapter = ArticleAdapter()
+        binding.articles.adapter = viewAdapter
+
+        asyncLoadNews()
     }
 
     override fun onResume() {
@@ -44,19 +53,17 @@ class MainActivity : AppCompatActivity() {
         }
         val articles = requests
             .filter { !it.isCancelled }
-            .flatMap { it.getCompleted() } 
+            .flatMap { it.getCompleted() }
 
-        val failed = requests.filter { it.isCancelled }
+        val failedCount = requests.filter { it.isCancelled }
             .size
 
-        val newsCount = findViewById<TextView>(R.id.newsCount)
-        val warnings = findViewById<TextView>(R.id.warnings)
-        val obtained = requests.size - failed
+        val obtained = requests.size - failedCount
 
-        GlobalScope.launch(Dispatchers.Main) {
-            newsCount.text = "Found ${articles.size} News" +
-                    " in ${obtained} feeds"
-            if (failed > 0) warnings.text = "Failed to fetch $failed feeds"
+        launch(Dispatchers.Main) {
+            binding.progressBar.visibility = View.GONE
+            Log.d("DEBUG", "articles개수:$articles")
+            viewAdapter.submitList(articles)
         }
     }
 
@@ -64,6 +71,7 @@ class MainActivity : AppCompatActivity() {
         feed: Feed,
         dispatcher: CoroutineDispatcher
     ) = GlobalScope.async(dispatcher) {
+        delay(1000)
         val builder = factory.newDocumentBuilder()
         val xml = builder.parse(feed.url)
         val news = xml.getElementsByTagName("channel").item(0)
@@ -75,7 +83,11 @@ class MainActivity : AppCompatActivity() {
             .filter { "item" == it.tagName }
             .map {
                 val title = it.getElementsByTagName("title").item(0).textContent
-                val summary = it.getElementsByTagName("description").item(0).textContent
+                var summary = it.getElementsByTagName("description").item(0).textContent
+                if(!summary.startsWith("<div")
+                    && summary.contains("<div")) {
+                    summary = summary.substring(0, summary.indexOf("<div"))
+                }
                 Article(feed.name, title, summary)
             }
             .toList()
