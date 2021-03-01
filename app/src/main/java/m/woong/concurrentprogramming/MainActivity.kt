c908fd84b2f92bd823d2c185ba7f5ce4364203bf
dlqkfzlr <hisunwoong@gmail.com>
2021-03-01 11:56:11 +0900

    Add data classes in model.kt

--- app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
+++ app/src/main/java/m/woong/concurrentprogramming/MainActivity.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import m.woong.concurrentprogramming.model.Article
+import m.woong.concurrentprogramming.model.Feed
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
     val feeds = listOf(
-        "https://www.npr.org/rss/rss.php?id=1001",
-        "http://rss.cnn.com/rss/cnn_topstories.rss",
-        "http://feeds.foxnews.com/foxnews/politics?format=xml",
-        "fake"
+        Feed("npr", "https://www.npr.org/rss/rss.php?id=1001"),
+        Feed("cnn", "http://rss.cnn.com/rss/cnn_topstories.rss"),
+        Feed("fox", "http://feeds.foxnews.com/foxnews/politics?format=xml"),
+        Feed("inv", "htt:myNewsFeed")
     )
     private val dispatcher = newFixedThreadPoolContext(2, "IO")
     private val factory = DocumentBuilderFactory.newInstance()
@@ -33,16 +35,16 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun asyncLoadNews() = GlobalScope.launch {
-        val requests = mutableListOf<Deferred<List<String>>>()
+        val requests = mutableListOf<Deferred<List<Article>>>()
         feeds.mapTo(requests) {
-            asyncFetchRssHeadlines(it, dispatcher)
+            asyncFetchArticles(it, dispatcher)
         }
         requests.forEach {  // 각각의 asyncFetchRssHeadlines가 모두 완료될 때까지 대기하는 코드
             it.join()
         }
-        val headlines = requests
+        val articles = requests
             .filter { !it.isCancelled }
-            .flatMap { it.getCompleted() }
+            .flatMap { it.getCompleted() } 
 
         val failed = requests.filter { it.isCancelled }
             .size
@@ -52,26 +54,30 @@ class MainActivity : AppCompatActivity() {
         val obtained = requests.size - failed
 
         GlobalScope.launch(Dispatchers.Main) {
-            newsCount.text = "Found ${headlines.size} News" +
-                    "in ${obtained} feeds"
+            newsCount.text = "Found ${articles.size} News" +
+                    " in ${obtained} feeds"
             if (failed > 0) warnings.text = "Failed to fetch $failed feeds"
         }
     }
 
-    private fun asyncFetchRssHeadlines(
-        feed: String,
+    private fun asyncFetchArticles(
+        feed: Feed,
         dispatcher: CoroutineDispatcher
     ) = GlobalScope.async(dispatcher) {
         val builder = factory.newDocumentBuilder()
-        val xml = builder.parse(feed)
+        val xml = builder.parse(feed.url)
         val news = xml.getElementsByTagName("channel").item(0)
         (0 until news.childNodes.length)
+            .asSequence()
             .map { news.childNodes.item(it) }
             .filter { Node.ELEMENT_NODE == it.nodeType }
             .map { it as Element }
             .filter { "item" == it.tagName }
             .map {
-                it.getElementsByTagName("title").item(0).textContent
+                val title = it.getElementsByTagName("title").item(0).textContent
+                val summary = it.getElementsByTagName("description").item(0).textContent
+                Article(feed.name, title, summary)
             }
+            .toList()
     }
 }
---
+++ app/src/main/java/m/woong/concurrentprogramming/model/model.kt
@@ -0,0 +1,12 @@
+package m.woong.concurrentprogramming.model
+
+data class Feed(
+    val name: String,
+    val url: String
+)
+
+data class Article(
+    val feed: String,
+    val title: String,
+    val summary: String
+)
